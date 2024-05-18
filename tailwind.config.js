ab6d5c1c0db69632faf9cdabd9a45ba6602ef259
Acorn <projectacorn@fumosoft.com>
2024-05-18 00:26:00 -0500

    ??

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {},
+    colors: {
+      'cirnoblue': '#294271',
+      'cirnolblue': '#b7e2ef',
+      'cirnored': '#960d14',
+      'cirnowhite': '#f1ecec',
+      'chalkwhite': '#e8e8e8',
+    },
+    fontFamily: {
+      'chalk': ['"Walter Turncoat"',],
+    }
+  },
+  plugins: [],
+}
+
